0e3ffaff70247a97b9e046a62ca6c15151f4b4ff
Oliver Bathurst <54957361+OliverMBathurst@users.noreply.github.com>
2026-01-10 17:13:17 +0000

    Reduce code usage

--- src/hooks/icon.tsx
+++ src/hooks/icon.tsx
@@ -4,14 +4,16 @@ import { getIcon } from "../helpers/icons"
 import { IIconProps } from "../icons"
 import { Context } from "../types/fs"
 
-const useIcon: (context: Context, noSelect?: boolean) => JSX.Element = (
+const useIcon: (context: Context, noSelect?: boolean, options?: IIconProps) => JSX.Element = (
 	context: Context,
-	noSelect: boolean = true
+	noSelect: boolean = true,
+	options
 ) => {
 	const Icon = useMemo(() => {
 		const props: IIconProps = {
 			className: noSelect ? NO_SELECT_CLASS : "",
-			draggable: false
+			draggable: false,
+			...options
 		}
 
 		return getIcon(context, props)
--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,91 +1,71 @@
 import React from "react"
+import cancel from "../icons/cancel.png"
+import close from "../icons/close.png"
+import drive from "../icons/drive.png"
+import executable from "../icons/executable.png"
 import file from "../icons/file.png"
 import folder from "../icons/folder.png"
-import drive from "../icons/drive.png"
-import textFile from "../icons/textFile.png"
-import executable from "../icons/executable.png"
 import internet from "../icons/internet.png"
-import pdf from "../icons/pdf.png"
-import close from "../icons/close.png"
-import minimise from "../icons/minimise.png"
 import maximise from "../icons/maximise.png"
-import start from "../icons/startMenu.png"
+import minimise from "../icons/minimise.png"
+import pdf from "../icons/pdf.png"
 import search from "../icons/search.png"
-import cancel from "../icons/cancel.png"
+import start from "../icons/startMenu.png"
+import textFile from "../icons/textFile.png"
 
-const GenericFileIcon = (props: IIconProps) => (
+const GenericFileIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={file} alt="File icon by icon8" {...props} />
 )
 
-const TextFileIcon = (props: IIconProps) => (
+const TextFileIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={textFile} alt="Text file icon by icon8" {...props} />
 )
 
-const ExecutableFileIcon = (props: IIconProps) => (
+const ExecutableFileIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={executable} alt="Executable file icon by icon8" {...props} />
 )
 
-const InternetIcon = (props: IIconProps) => (
+const InternetIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={internet} alt="File icon by icon8" {...props} />
 )
 
-const PdfIcon = (props: IIconProps) => (
+const PdfIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={pdf} alt="PDF icon by icon8" {...props} />
 )
 
-const FolderIcon = (props: IIconProps) => (
+const FolderIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={folder} alt="Folder icon by icon8" {...props} />
 )
 
-const DriveIcon = (props: IIconProps) => (
+const DriveIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={drive} alt="Drive icon by icon8" {...props} />
 )
 
-const StartMenuIcon = (props: IIconProps) => (
+const StartMenuIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={start} alt="Start menu icon by icon8" {...props} />
 )
 
-const CloseIcon = (props: IIconProps) => (
+const CloseIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={close} alt="Close icon by icon8" {...props} />
 )
 
-const MinimizeIcon = (props: IIconProps) => (
+const MinimizeIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={minimise} alt="Minimise icon by icon8" {...props} />
 )
 
-const MaximizeIcon = (props: IIconProps) => (
+const MaximizeIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={maximise} alt="Maximise icon by icon8" {...props} />
 )
 
-const SearchIcon = (props: IIconProps) => (
+const SearchIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={search} alt="Search icon by icon8" {...props} />
 )
 
-const CancelIcon = (props: IIconProps) => (
+const CancelIcon = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
 	<img src={cancel} alt="Cancel icon by icon8" {...props} />
 )
 
 export {
-	GenericFileIcon,
-	CloseIcon,
-	MinimizeIcon,
-	MaximizeIcon,
-	InternetIcon,
-	PdfIcon,
-	TextFileIcon,
-	ExecutableFileIcon,
-	FolderIcon,
-	DriveIcon,
-	StartMenuIcon,
-	SearchIcon,
-	CancelIcon
-}
-
-export interface IIconProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-	className?: string
-	draggable?: boolean
-	onClick?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
-	onMouseDown?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
-	onMouseUp?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
-	onMouseMove?: (e: React.MouseEvent<HTMLImageElement, MouseEvent>) => void
+    CancelIcon, CloseIcon, DriveIcon, ExecutableFileIcon,
+    FolderIcon, GenericFileIcon, InternetIcon, MaximizeIcon, MinimizeIcon, PdfIcon, SearchIcon, StartMenuIcon, TextFileIcon
 }
